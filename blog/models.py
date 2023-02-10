c018818e05fbb850ad61e29b389209fb0169227e
Chirag Simkhada <chiragsimkhada@gmail.com>
2023-02-10 20:28:20 +0545

    Viewed blogs in frontend

--- blog/models.py
+++ blog/models.py
@@ -8,3 +8,6 @@ class BlogModel(models.Model):
     content = models.TextField()
     user = models.ForeignKey(to=User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import BlogModel
 
 
 def home(request):
-    return render(request, "blog/index.html")
+    blogs = BlogModel.objects.all()
+    return render(request, "blog/index.html", {"blogs":blogs})
 
 def about(request):
     return HttpResponse("About Page")
